455eb514d2e3d7d3ac0a3c25928602f44eb551c2
Young Cha <youngc0905@gmail.com>
2019-12-01 22:17:50 -0800

    fixed serverside code to work

--- server/model.js
+++ server/model.js
@@ -1,5 +1,16 @@
-const Stock = require('../database/seed.js')
+const Sequelize = require('sequelize');
 
+const sequelize = new Sequelize('robinhoodclone', 'root', 'young', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+const Stock = sequelize.define('Stock', {
+  stock_name: Sequelize.STRING,
+  stock_symbol: Sequelize.STRING,
+  owner: Sequelize.STRING,
+  price: Sequelize.STRING
+})
+Stock.sync();
 module.exports = {
   getStockData: (callback) => {
     Stock.findAll()
